ee6620a6e0dfa36879579f1353d9591c8ad40c48
EugineI <eugineindimuli@gmail.com>
2024-12-05 11:04:20 -0800

    all

---
+++ 15-binary_tree_is_full.c
@@ -0,0 +1,17 @@
+#include"binary_trees.h"
+/**
+ * binary_tree_is_full - checks if a tree is full
+ * @tree: tree to check
+ * Return: 0 or 1
+ */
+int binary_tree_is_full(const binary_tree_t *tree)
+{
+	if (tree == NULL)
+		return (0);
+	if (tree->left == NULL && tree->right == NULL)
+		return (1);
+	if (tree->left != NULL && tree->right != NULL)
+		return (binary_tree_is_full(tree->left) &&
+				binary_tree_is_full(tree->right));
+	return (0);
+}
